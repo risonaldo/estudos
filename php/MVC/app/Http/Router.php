ee940fa4d36be3786db40c61b5d0670645098e95
Risonaldo <risonaldomaciel@hotmail.com>
2024-01-25 13:06:58 -0400

    criando routes

---
+++ php/MVC/app/Http/Router.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http;
+
+class Router
+{
+    private $url = '';
+    private $prefix = '';
+    private $routes = [];
+    private $request;
+
+    public function __construct($url)
+    {
+        $this->request = new Request();
+        $this->url = $url;
+        $this->setPrefix();
+    }
+
+    private function setPrefix()
+    {
+        $parseUrl = parse_url($this->url);
+
+        echo "<pre>";
+        print_r($parseUrl);
+        echo "</pre>";
+    }
+}
--- php/MVC/index.php
+++ php/MVC/index.php
@@ -1,8 +1,17 @@
-<?php 
+<?php
 
-require __DIR__ .'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
+define('URL', 'http://localhost:8888');
+
+use \App\Http\Router;
 use \App\Controller\Pages\Home;
 
+$obRouter = new Router(URL);
 
+echo "<pre>";
+print_r($obRouter);
+echo "</pre>";
+
+exit;
 echo Home::getHome();
