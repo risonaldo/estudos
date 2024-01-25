34b1e0deac7e26ed2c328297fef2c03f3584ac37
Risonaldo <risonaldomaciel@hotmail.com>
2024-01-25 09:45:24 -0400

    esqueleto mvc criado

--- php/MVC/app/Controller/Pages/Home.php
+++ php/MVC/app/Controller/Pages/Home.php
@@ -3,15 +3,19 @@
 namespace App\Controller\Pages;
 
 use \App\Utils\View;
+use \App\Model\Entity\Organization;
 
-class Home
+class Home extends Page
 {
 
     public static function getHome()
     {
-        return View::render('pages/home', [
-            'name' => 'Risonaldo - Maciel',
-            'description' => 'MVC'
+        $obOrganization = new Organization();
+        $content = View::render('pages/home', [
+            'name' => $obOrganization->name,
+            'description' => $obOrganization->descripton,
+            'site' => $obOrganization->site
         ]);
+        return parent::getPage('Risonaldo - HOME', $content);
     }
 }
---
+++ php/MVC/app/Model/Entity/Organization.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model\Entity;
+
+class Organization
+{
+    public $id = 1;
+    public $name = 'Organization';
+    public $site = 'Site.com.br';
+    public $descripton ='Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid porro esse eligendi cum debitis fuga perspiciatis tempore eum consequatur delectus, illum, ullam vitae quos. Veritatis quisquam ipsam culpa fugiat maxime.
+    ';
+
+}
